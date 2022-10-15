e840b714b106c9b19c18a0038d0032660c378d6b
Hickups789 <jackysantiago358@yahoo.com>
2022-10-15 16:32:34 -0400

    added concurrently

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
-
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+});
