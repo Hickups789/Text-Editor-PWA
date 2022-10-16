6dab1e6cb55457ca0e72993094e8a7f69bc32e16
Hickups789 <jackysantiago358@yahoo.com>
2022-10-16 16:09:44 -0400

    built

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,18 @@
 import { openDB } from 'idb';
+import 'regenerator-runtime/runtime';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+export const initdb = async () => {
+  // We are creating a new database named 'contact_db' which will be using version 1 of the database.
+  openDB('contact_db', 1, {
+    // Add our database schema if it has not already been initialized.
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('contacts')) {
+        console.log('contacts store already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
-
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
-initdb();
+      // Create a new object store for the data and give it a key name of 'id' which will increment automatically
+      db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true });
+      console.log('contacts store created');
+    }
+  })
+}
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,33 +1,21 @@
-import { Workbox } from 'workbox-window';
-import Editor from './editor';
-import './database';
-import '../css/style.css';
+import "./form";
+import "./submit";
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const main = document.querySelector('#main');
-main.innerHTML = '';
+import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
 
-const loadSpinner = () => {
-  const spinner = document.createElement('div');
-  spinner.classList.add('spinner');
-  spinner.innerHTML = `
-  <div class="loading-container">
-  <div class="loading-spinner" />
-  </div>
-  `;
-  main.appendChild(spinner);
-};
+import { initdb } from './database';
 
-const editor = new Editor();
+window.addEventListener('load', function() {
+  initdb();
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
 
-if (typeof editor === 'undefined') {
-  loadSpinner();
-}
 
-// Check if service workers are supported
-if ('serviceWorker' in navigator) {
-  // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
-  workboxSW.register();
-} else {
-  console.error('Service workers are not supported in this browser.');
-}
+    // Import CSS files
+    import "../css/index.css";
